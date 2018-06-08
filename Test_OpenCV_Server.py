fed3c18d969d496b02c5cba526cbf8eea4682ef4
zhuo_hf@foximail.com <zhuo_hf@foximail.com>
2018-06-08 22:37:41 +0800

    add opencv support

---
+++ Test_OpenCV_Server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/8 10:59
+# @Author  : zhuo_hf@foxmail.com
+# @Site    : 
+# @File    : Test_Server.py
+# @Software: PyCharm
+import socket
+import cv2
+import os,sys,pygame
+#from pygame.locals import *
+import numpy
+
+svrsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+svrsocket.bind(("192.168.0.18", 6000))
+print("Start the service ....")
+while 1:
+    stringData, address = svrsocket.recvfrom(65500)
+    #print("recv the data={}".format(len(stringData)))
+    data = numpy.fromstring(stringData, dtype='uint8')
+    img = cv2.imdecode(data, 1)
+    cv2.imshow("Dst Img", img)  # 显示处理后的函数
+    if cv2.waitKey(10) & 0xff == 27: # ESC键
+        break
+
+svrsocket.close()
+cv2.destroyAllWindows()
--- Test_Server.py
+++ Test_Server.py
@@ -16,16 +16,16 @@ pygame.display.set_caption("web cam")
 
 pygame.display.flip()
 svrsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-svrsocket.bind(("10.125.156.227", 6000))
+svrsocket.bind(("192.168.0.18", 6000))
 clock = pygame.time.Clock()    #计算帧速
 while 1:
-    print("Start the service ....")
-    data, address = svrsocket.recvfrom(80000*4)
-    print("recv the data")
-    camshot = pygame.image.frombuffer(data, (320,240), "RGB")
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-    screen.blit(camshot, (0,0))
-    pygame.display.update()
-    print(clock.get_fps())    #在终端打印帧速
+    #print("Start the service ....")
+    data, address = svrsocket.recvfrom(65500)
+    print("recv the data={}".format(len(data)))
+    # camshot = pygame.image.frombuffer(data, (320,240), "RGB")
+    # for event in pygame.event.get():
+    #         if event.type == pygame.QUIT: sys.exit()
+    # screen.blit(camshot, (0,0))
+    # pygame.display.update()
+    #print(clock.get_fps())    #在终端打印帧速
     clock.tick()
