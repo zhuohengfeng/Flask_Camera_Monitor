858a3536d72408ff3230b65fed290390233b8048
zhuohf1 <zhuohf1@lenovo.com>
2018-06-08 15:37:21 +0800

    change the IP&port

--- Test_Server.py
+++ Test_Server.py
@@ -11,16 +11,18 @@ import os,sys,pygame
 #from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((160,120))
+screen = pygame.display.set_mode((320,240))
 pygame.display.set_caption("web cam")
 
 pygame.display.flip()
 svrsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-svrsocket.bind(("127.0.0.1", 1234))
+svrsocket.bind(("10.125.156.227", 6000))
 clock = pygame.time.Clock()    #计算帧速
 while 1:
-    data, address = svrsocket.recvfrom(80000)
-    camshot = pygame.image.frombuffer(data, (160,120), "RGB")
+    print("Start the service ....")
+    data, address = svrsocket.recvfrom(80000*4)
+    print("recv the data")
+    camshot = pygame.image.frombuffer(data, (320,240), "RGB")
     for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
     screen.blit(camshot, (0,0))
